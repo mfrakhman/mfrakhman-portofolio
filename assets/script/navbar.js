2f4432f0d9724e78f0d3b1672bd9cc1728bf2be6
mfrakhman <rrrfajar1@gmail.com>
2023-08-23 03:00:45 +0700

    feature: navbar hide on scroll

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -34,6 +34,17 @@ document.addEventListener("click", function (e) {
   }
 });
 
+var prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
 // 2nd Version
 // const navItems = document.querySelectorAll(".nav-item");
 
